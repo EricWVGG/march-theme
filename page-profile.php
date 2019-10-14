7a753cd6f743664e7d26c278001c0caad34be524
EricWVGG <eric@whiskyvangoghgo.com>
2019-10-14 18:15:52 +0000

    cleanup, no longer uses MCB

--- page-profile.php
+++ page-profile.php
@@ -1,7 +1,7 @@
-<?php /* Template Name: Profile */ 
+<?php /* Template Name: Profile */
 
 get_header(); ?>
-	
+
 <div class="ajax_replaceable" id="content">
 
   	<section class="header header_sub header_sub_about">
@@ -11,29 +11,29 @@ get_header(); ?>
             </nav>
         </header>
     </section>
-  
+
 	  <section role="main" id="main_section">
-  	
+
       	<?php if (have_posts()): while (have_posts()) : the_post(); ?>
-      	
+
         		<article id="post-<?php the_ID(); ?>" class="<?=implode(' ', get_post_class('article_profile'))?>">
-        		
+
                 <!-- h1><?php the_title(); ?></h1 -->
-    	
+
           			<?php the_content(); ?>
-        			
+
         		</article>
-        		
+
         		<article id="about_clients">
-        		
+
                 <h2>Select Clients</h2>
-        		    <?php the_block('clients') ?>
+        		    <?php echo get_field('clients');?>
         		</article>
-      		
+
       	<?php endwhile; endif; ?>
-  	
+
   	</section>
 
 </div>
-	
+
 <?php get_footer(); ?>
--- project_grid.php
+++ project_grid.php
@@ -2,7 +2,7 @@
 <? if(NOT_AJAX || (!NOT_AJAX && !isset($detail_view_id))) : ?>
 
 <div class="ajax_replaceable projects_page" id="content">
-	
+
     <section class="header header_sub header_sub_projects">
         <header>
             <nav>
@@ -13,30 +13,29 @@
 
 
   	<section role="main" id="main_section" >
-  	
-      	<?php 
+
+      	<?php
       	$page_content = new WP_Query(array('page_id' => $grid_page_id));
-      	if ($page_content->have_posts()): while ($page_content->have_posts()) : $page_content->the_post(); 
+      	if ($page_content->have_posts()): while ($page_content->have_posts()) : $page_content->the_post();
       	?>
-      	
+
         <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
             <h2><?php the_title(); ?>. &nbsp; </h2>
             <p><?php the_content(); ?></p>
             <div class="clearfix"></div>
-            <?php $clients = get_the_block('clients') ?>
         </article>
-      		
+
       	<?php endwhile; endif; ?>
 
         <article id="featured">
             <ul>
-      		    <?php 
+      		    <?php
         		      $images = get_field($acf_field_name);
         		      if( $images ) foreach ( $images AS $image ) :
             		      $img = $image['image'];
             		      $attch_id = get_post_thumbnail_id( $img->ID );
             		      if(!$attch_id) continue;
-                      $photo = wp_get_attachment_image_src($attch_id, 'large'); 
+                      $photo = wp_get_attachment_image_src($attch_id, 'large');
                       $photo[2] = ( 316 * $photo[2] ) / $photo[1];
                       $photo[1] = 316;
                 ?>
@@ -48,12 +47,12 @@
                 <?php endforeach; ?>
             </ul>
         </article>
-        
+
         <article id="case_studies">
             <div id="case_studies_scroll_target" ></div>
             <h2>Case Studies</h2>
             <ul class="case_studies">
-      		    <?php 
+      		    <?php
                 $case_studies = get_field('case_studies');
                 if( $case_studies ) : foreach( $case_studies AS $case_study ) :
                     $post = $case_study;
@@ -89,13 +88,13 @@
                 <?php endforeach; endif; wp_reset_query();?>
             </ul>
         </article>
-        
+
         <article id="clients">
             <div id="clients_scroll_target" ></div>
             <h2>Select Clients</h2>
-            <?=$clients;?>
+            <?php echo get_field('clients');?>
         </article>
-        
+
         <script type="text/javascript">
           $(function() {
             setTimeout(function() {
@@ -103,7 +102,7 @@
             }, 2400);
           });
         </script>
-  	
+
   	</section>
 
 <? endif; ?>
